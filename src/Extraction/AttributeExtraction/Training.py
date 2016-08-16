c100fae954f7b5e30f63ae1d352ca25d1a371ddb
wlane <will@asklane.com>
2016-08-16 10:08:46 -0700

    attribute extraction models generated

--- src/Extraction/AttributeExtraction/Training.py
+++ src/Extraction/AttributeExtraction/Training.py
@@ -1,7 +1,11 @@
 import subprocess
 import re
+import os
 from SentenceTokenizer import strip_sec_headers_tokenized_text
+from SystemUtilities.Configuration import STANFORD_NER_PATH, ATTRIB_EXTRACTION_DIR_HOME
 
+
+# TODO Move these globals to the globals file
 features = [
     "useClassFeature=true",
     "useWord=true",
@@ -30,80 +34,87 @@ entity_types = [
 ]
 
 
-def train(training_doc_objs, path="EntityExtractor/",
-          stanford_ner_path="/home/wlane/stanford-ner-2015-04-20/stanford-ner.jar",
-          train_script_name="train_model.sh"):
+def train(patients, model_path=ATTRIB_EXTRACTION_DIR_HOME, script_path="Extraction\\AttributeExtraction\\",
+          stanford_ner_path=STANFORD_NER_PATH,
+          train_script_name="train_model.bat"):
     global features
     global entity_types
+
+    # if os.environ['OS'] == "Windows_NT":
+    #     train_script_name = "train_model.bat"
+
+    training_doc_objs = get_documents_from_patients(patients)
+
     for type in entity_types:
-        if type != "Status":
-            train_file_name = path + "Train-Files/" + "train-" + type + ".tsv"
-            prop_file_name = path + "Prop-Files/" + type + ".prop"
-            model_name = path + "Models/" + "model-" + type + ".ser.gz"
-            create_train_file(training_doc_objs, train_file_name, type)
-            create_prop_file(prop_file_name, train_file_name, features, model_name)
-            train_model(stanford_ner_path, prop_file_name, path + train_script_name)
+        train_file_name = model_path + r"Train-Files\\" + "train-" + type + ".tsv"
+        prop_file_name = model_path + r"Prop-Files\\" + type + ".prop"
+        model_name = model_path + r"Models\\" + "model-" + type + ".ser.gz"
+        create_train_file(training_doc_objs, train_file_name, type)
+        create_prop_file(prop_file_name, train_file_name, features, model_name)
+        train_model(stanford_ner_path, prop_file_name, script_path+train_script_name)
 
 
-def create_train_file(training_doc_objs, train_file_name, type):
+def create_train_file(training_sent_objs, train_file_name, type):
     """
     Sorry about the crazy embedded FOR loops and indents.
     I will modularize better to make it prettier.
     """
     train_file = open(train_file_name, 'w')
-    for doc in training_doc_objs:
-        doc_obj = training_doc_objs[doc]
-        for sent_obj in doc_obj.get_sentence_obj_list():
-            if sent_obj.has_substance_abuse_entity():
-                sentence = sent_obj.sentence
-                entity_set = sent_obj.set_entities
-                sent_offset = sent_obj.begin_idx
+    for sent_obj in training_sent_objs:
+        if len(sent_obj.gold_events) > 0: # if the sentence is predicted to have an event
+            sentence = sent_obj.text
+            gold_event_set = sent_obj.gold_events
+            sent_offset = sent_obj.span_in_doc_start
 
-                for match in re.finditer("\S+", sentence):
-                    start = match.start()
-                    end = match.end()
-                    pointer = sent_offset + start
-                    word = match.group(0).rstrip(",.:;")
-                    if word not in {"SOCIAL", "HISTORY", "SUBSTANCE",
+            for match in re.finditer("\S+", sentence):
+                start = match.start()
+                end = match.end()
+                pointer = sent_offset + start
+                word = match.group(0).rstrip(",.:;")
+                if word not in {"SOCIAL", "HISTORY", "SUBSTANCE",
                                     "ABUSE"}:  # see tokenizer in utils, they must both match
-                        train_file.write(word)
-                        # Debug line
-                        # train_file.write("[" + str(pointer) + "," + str(sent_offset + match.end()) + "]")
-                        train_file.write("\t")
-                        answer = "0"
-                        debug_str = ""
-                        for entity in entity_set:
-                            if answer != "0":
-                                break
-                            if entity.is_substance_abuse():
-                                attr_dict = entity.dict_of_attribs
-                                for attr in attr_dict:
-                                    attr_start = int(attr_dict[attr].span_begin)
-                                    attr_end = int(attr_dict[attr].span_end)
-                                    if attr_dict[attr].type == type and \
-                                                            attr_start <= pointer < attr_end:
-                                        answer = type
-                                        # Debug lines
-                                        # answer += "\t" + attr_dict[attr].text +\
-                                        #          "[" + str(attr_start) +\
-                                        #          "," + str(attr_end) + "]"
-                                        debug_str = "--- Sent obj start index: " + str(sent_offset) + "\n" + \
-                                                    "--- Match obj start index: " + str(start) + "\n" + \
-                                                    "--- Match obj end index: " + str(end) + "\n" + \
-                                                    "--- Pointer index: " + str(sent_offset) + " + " + \
-                                                    str(start) + " = " + str(pointer) + "\n" + \
-                                                    "--- Attr start index: " + str(attr_start) + "\n" + \
-                                                    "--- Attr end index: " + str(attr_end) + "\n"
-                                        break
-                        train_file.write(answer + "\n")
-                        # Debug line
-                        # train_file.write(debug_str)
-                        # print(debug_str)
-
+                    train_file.write(word)
+                    # Debug line
+                    # train_file.write("[" + str(pointer) + "," + str(sent_offset + match.end()) + "]")
+                    train_file.write("\t")
+                    answer = "0"
+                    debug_str = ""
+                    for entity in gold_event_set:
+                        if answer != "0":
+                            break
+                        attr_dict = entity.attributes
+                        for attr in attr_dict:
+                            for span in attr_dict[attr].spans:
+                                attr_start = int(span.start)
+                                attr_end = int(span.stop)
+                                if attr_dict[attr].type == type and \
+                                                        attr_start <= pointer < attr_end:
+                                    answer = type
+                                    # Debug lines
+                                    # answer += "\t" + attr_dict[attr].text +\
+                                    #          "[" + str(attr_start) +\
+                                    #          "," + str(attr_end) + "]"
+                                    debug_str = "--- Sent obj start index: " + str(sent_offset) + "\n" + \
+                                                "--- Match obj start index: " + str(start) + "\n" + \
+                                                "--- Match obj end index: " + str(end) + "\n" + \
+                                                "--- Pointer index: " + str(sent_offset) + " + " + \
+                                                str(start) + " = " + str(pointer) + "\n" + \
+                                                "--- Attr start index: " + str(attr_start) + "\n" + \
+                                                "--- Attr end index: " + str(attr_end) + "\n"
+                                    break
+                    train_file.write(answer + "\n")
+                    # Debug line
+                    # train_file.write(debug_str)
+                    # print(debug_str)
     train_file.close()
 
 
 def create_prop_file(prop_file_name, train_file_name, features, model_name):
+    # Windows ONLY: replace '\' with '\\'. Yes its very stupid, but so is Windows.
+    # train_file_name = train_file_name.replace(r"\\", r"\\\\") # of course, because windows is stupid, 1==4 and 2==8
+    # model_name = model_name.replace(r"\\", r"\\\\")
+    # End stupid Windows fix
+
     prop_file = open(prop_file_name, 'w')
     prop_file.write("trainFile = " + train_file_name + "\n")
     prop_file.write("serializeTo = " + model_name + "\n")
@@ -115,4 +126,13 @@ def create_prop_file(prop_file_name, train_file_name, features, model_name):
 
 
 def train_model(stanford_ner_path, prop_file_name, train_script_name):
-    subprocess.call(["./" + train_script_name + " " + stanford_ner_path + " " + prop_file_name], shell=True)
+    subprocess.call([train_script_name, stanford_ner_path, prop_file_name], shell=True)
+
+def get_documents_from_patients(patients):
+    sentences = list()
+    for patient in patients:
+        for document in patient.doc_list:
+            for sent_obj in document.sent_list:
+                if (len(sent_obj.gold_events)) > 0:  # if the sentence has an event
+                    sentences.append(sent_obj)
+    return sentences
